1665a5aa663b0b71d771163cffd65e8c1b58502e
Rahul Grover <50244358+rgrover798@users.noreply.github.com>
2023-12-09 01:21:47 -0600

    Add files via upload

---
+++ mp_traversals/src/ImageTraversal.cpp
@@ -0,0 +1,138 @@
+#include <cmath>
+#include <iterator>
+#include <iostream>
+
+#include "cs225/HSLAPixel.h"
+#include "cs225/PNG.h"
+#include "Point.h"
+
+#include "ImageTraversal.h"
+
+namespace Traversals {
+  /**
+  * Calculates a metric for the difference between two pixels, used to
+  * calculate if a pixel is within a tolerance.
+  *
+  * @param p1 First pixel
+  * @param p2 Second pixel
+  * @return the difference between two HSLAPixels
+  */
+  double calculateDelta(const HSLAPixel & p1, const HSLAPixel & p2) {
+    double h = fabs(p1.h - p2.h);
+    double s = p1.s - p2.s;
+    double l = p1.l - p2.l;
+
+    // Handle the case where we found the bigger angle between two hues:
+    if (h > 180) { h = 360 - h; }
+    h /= 360;
+
+    return sqrt( (h*h) + (s*s) + (l*l) );
+  }
+  
+  /**
+  * Adds a Point for the bfs traversal to visit at some point in the future.
+  * @param work_list the structure which stores the list of points which need to be visited by the traversal
+  * @param point the point to be added
+  */
+  void bfs_add(std::deque<Point> & work_list, const Point & point) {
+    /** @todo [Part 1] */
+  }
+
+  /**
+  * Adds a Point for the dfs traversal to visit at some point in the future.
+  * @param work_list the structure which stores the list of points which need to be visited by the traversal
+  * @param point the point to be added
+  */
+  void dfs_add(std::deque<Point> & work_list, const Point & point) {
+    /** @todo [Part 1] */
+  }
+
+  /**
+  * Removes and returns the current Point in the bfs traversal
+  * @param work_list the structure which stores the list of points which need to be visited by the traversal
+  */
+  Point bfs_pop(std::deque<Point> & work_list) {
+    /** @todo [Part 1] */
+    return Point(0, 0);
+  }
+
+  /**
+  * Removes and returns the current Point in the dfs traversal
+  * @param work_list the structure which stores the list of points which need to be visited by the traversal
+  */
+  Point dfs_pop(std::deque<Point> & work_list) {
+    /** @todo [Part 1] */
+    return Point(0, 0);
+  }
+
+  /**
+  * Initializes a ImageTraversal on a given `png` image,
+  * starting at `start`, and with a given `tolerance`.
+  * @param png The image this traversal is going to traverse
+  * @param start The start point of this traversal
+  * @param tolerance If the current point is too different (difference larger than tolerance) with the start point,
+  * it will not be included in this traversal
+  * @param traversal_add a function pointer to an implemented function which takes in a reference to the work list
+  * and the point to be added
+  * @param traversal_pop a function pointer to an implemented function which takes in a reference to the work list
+  * and returns the next point to be processed in the traversal
+  */
+  ImageTraversal::ImageTraversal(const PNG & png, const Point & start, double tolerance, add_function traversal_add, pop_function traversal_pop) {  
+    /** @todo [Part 1] */
+  }
+
+  /**
+  * Returns an iterator for the traversal starting at the first point.
+  */
+  ImageTraversal::Iterator ImageTraversal::begin() {
+    /** @todo [Part 1] */
+    return ImageTraversal::Iterator();
+  }
+
+  /**
+  * Returns an iterator for the traversal one past the end of the traversal.
+  */
+  ImageTraversal::Iterator ImageTraversal::end() {
+    /** @todo [Part 1] */
+    return ImageTraversal::Iterator();
+  }
+
+  /**
+  * Default iterator constructor.
+  */
+  ImageTraversal::Iterator::Iterator() {
+    /** @todo [Part 1] */
+  }
+
+
+  /**
+  * Iterator increment opreator.
+  *
+  * Advances the traversal of the image.
+  */
+  ImageTraversal::Iterator & ImageTraversal::Iterator::operator++() {
+    /** @todo [Part 1] */
+    return *this;
+  }
+
+  /**
+  * Iterator accessor opreator.
+  *
+  * Accesses the current Point in the ImageTraversal.
+  */
+  Point ImageTraversal::Iterator::operator*() {
+    /** @todo [Part 1] */
+    return Point(0, 0);
+  }
+
+  /**
+  * Iterator inequality operator.
+  *
+  * Determines if two iterators are not equal.
+  */
+  bool ImageTraversal::Iterator::operator!=(const ImageTraversal::Iterator &other) {
+    /** @todo [Part 1] */
+    return false;
+  }
+
+}
